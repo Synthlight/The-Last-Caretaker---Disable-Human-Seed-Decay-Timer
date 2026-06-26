40ca285895c6d08c6f88ac5ffe4315c5fbaa7e9e
Gregory Brutsche <gregory@mail.brutsches.local>
2026-06-26 11:37:46 -0400

    Update for game v0.8.0.612115.

--- dllmain.cpp
+++ dllmain.cpp
@@ -8,7 +8,7 @@
 #include "PatchThreading.h"
 #include "Util.h"
 
-// For v0.6.0.606504.
+// For v0.8.0.612115.
 
 BOOL APIENTRY DllMain(const HMODULE hModule, const DWORD ulReasonForCall, const LPVOID lpReserved) {
     SetupLog(GetLogPathAsCurrentDllDotLog());
@@ -22,11 +22,17 @@ BOOL APIENTRY DllMain(const HMODULE hModule, const DWORD ulReasonForCall, const 
 
 bool DisableHumanSeedDecayTimer(const std::string& moduleName, const PTR_SIZE moduleAddress, AllocateMemory* allocator, LogBuffer* logBuffer) {
     // Search for seconds as a float.
-    return DoSimplePatch(moduleName, moduleAddress, "Disable Human Seed Decay Timer", "F3 0F 5C FE F3 0F 11 BB 20 03 00 00", FOUR_BYTE_NOP, logBuffer);
+    /*
+    VoyageSteam-Win64-Shipping.exe+513A8A6 - F3 0F10 8B 20030000   - movss xmm1,[rbx+00000320]
+    VoyageSteam-Win64-Shipping.exe+513A8AE - 48 8B CB              - mov rcx,rbx
+    VoyageSteam-Win64-Shipping.exe+513A8B1 - F3 0F5C CE            - subss xmm1,xmm6 // <------ NOP
+    VoyageSteam-Win64-Shipping.exe+513A8B5 - F3 0F11 8B 20030000   - movss [rbx+00000320],xmm1
+    */
+    return DoSimplePatch(moduleName, moduleAddress, "Disable Human Seed Decay Timer", "F3 0F 5C ?? F3 0F 11 ?? 20 03 00 00", FOUR_BYTE_NOP, logBuffer);
 }
 
 void DoInjection() {
-    LOG("Initializing v1.2.0.");
+    LOG("Initializing v1.3.0.");
 
     auto moduleName   = "VoyageSteam-Win64-Shipping.exe";
     auto moduleHandle = GetModuleHandle(moduleName);
@@ -39,7 +45,7 @@ void DoInjection() {
 
     if (moduleHandle == nullptr) {
         LOG("Unable to find module, aborting.");
-        MessageBoxW(nullptr, L"Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
+        MessageBoxW(nullptr, L"Disable-Human-Seed-Decay-Timer Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
         return;
     }
 
@@ -52,7 +58,7 @@ void DoInjection() {
     AllocateMemory allocator;
     /*
     if (!allocator.AllocateGlobalAddresses(moduleName, moduleAddress)) {
-        MessageBoxW(nullptr, L"Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
+        MessageBoxW(nullptr, L"Disable-Human-Seed-Decay-Timer Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
         return;
     }
     */
@@ -68,7 +74,7 @@ void DoInjection() {
     if (result) {
         //MessageBoxW(nullptr, L"Patching done!", L"Patching Done", MB_ICONINFORMATION | MB_OK);
     } else {
-        MessageBoxW(nullptr, L"Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
+        MessageBoxW(nullptr, L"Disable-Human-Seed-Decay-Timer Patching failed.", L"Patching Failed", MB_ICONERROR | MB_OK);
     }
 
     LOG("Patching done!");
